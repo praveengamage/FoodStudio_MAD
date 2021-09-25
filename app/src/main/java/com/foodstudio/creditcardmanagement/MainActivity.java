0ca28c6477dc4dd22cecb173ecf1736200312a5b
praveengamage <48019543+praveengamage@users.noreply.github.com>
2021-09-26 01:11:53 +0530

    Initial

---
+++ app/src/main/java/com/foodstudio/creditcardmanagement/MainActivity.java
@@ -0,0 +1,30 @@
+package com.foodstudio.creditcardmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.cardmanage);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ManageCards.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
